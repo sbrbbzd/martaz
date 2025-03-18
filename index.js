6207cc5fb255b4ce6012694c9ab171eb11938870
Sabir <sbrbbzd@gmail.com>
2025-03-18 22:29:24 +0400

    Initial commit

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+// index.js
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'frontend/dist' directory
+// Serve static files from the frontend/dist directory
 app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
 // For any request that doesn't match a static file, serve the index.html
+// This is needed for client-side routing to work properly
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-}); 
+});
